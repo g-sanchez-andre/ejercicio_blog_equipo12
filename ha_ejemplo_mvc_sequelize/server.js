72bec6d883b5b5a10d9ba9c39a709d0c1290a61d
JuanEstevess <jignacioesteves@gmail.com>
2023-05-04 15:03:10 -0300

    second commit

--- ha_ejemplo_mvc_sequelize/server.js
+++ ha_ejemplo_mvc_sequelize/server.js
@@ -5,8 +5,8 @@ const routes = require("./routes");
 const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 
-app.use(methodOverride("_method"));
-app.use(express.static("public"));
+//app.use(methodOverride("_method"));
+//app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
