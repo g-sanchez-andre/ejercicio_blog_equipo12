e3cd514d53ea157c6408b52516333a25a4a13d24
andres-mps <andresmps@gmail.com>
2023-05-04 16:35:07 -0300

    crear router

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -6,9 +6,7 @@ const articleController = require("../controllers/articleController");
 // ...
 
 router.get("/", articleController.index);
-router.get("/crear", (req, res) => {
-  return res.send("ok");
-});
+router.get("/crear", articleController.create);
 router.post("/", articleController.store);
 router.get("/:id", articleController.show);
 router.get("/:id/editar", articleController.edit);
