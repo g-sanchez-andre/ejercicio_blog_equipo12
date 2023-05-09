9cf31a9960b3acc1a308b5bdf15284f9e2c75bee
Juan Andrés Chítaro <juanchitaro@gmail.com>
2023-05-09 17:10:22 -0300

    trying passport setup

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 
+const {User} = require("./models");
+
 const methodOverride = require("method-override");
 app.use(
   session({
@@ -19,13 +21,50 @@ app.use(
 
 app.use(passport.session());
 
-passport.use(
-  new LocalStrategy((username, password, cb) => {
-    // Aquí adentro es necesario validar (contra nuestra base de datos)
-    // que username y password sean correctos.
-    // Ver la documentación de Passport por detalles.
-  }),
+passport.use(new LocalStrategy({
+usernameField: 'email',
+passwordField: 'password'
+},
+  async(username, password, done) => {
+  try{
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      console.log("El usuario no existe");
+      return done(null, false, { message: "Credenciales incorrectas."});
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      console.log("La contraseña es inválida.");
+      return done(null, false, { message: "Credenciales incorrectas." })
+  } catch (error) {
+    return done(error);
+  }
+}})
 );
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+  });
+  
+  passport.deserializeUser(async (id, done) => {
+  try {
+  const user = await User.findByPk(id);
+  done(null, user); // Usuario queda disponible en req.user.
+  } catch (err) {
+  done(err);
+  }
+  });
+   
+const user = await User.findOne({ where: { username } });
+    if (!user) {
+      console.log("El usuario no existe");
+      return done(null, false, { message: "Credenciales incorrectas."})
+    }
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      console.log("La contraseña es inválida.");
+      return done(null, false, { message: "Credenciales incorrectas." })
+  }
 
 app.use(methodOverride("_method"));
 app.use(express.static("public"));
